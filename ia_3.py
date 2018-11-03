7a9dbfe4ea904627a274b6cffd8838370d411bd4
Luis Carlos <luiscarlos.sf@outlook.com>
2018-11-03 00:52:25 -0300

    Mudando o algoritmo

--- ia_3.py
+++ ia_3.py
@@ -4,115 +4,102 @@ pessoa joga futebol ou tênis. As entradas devem ocorrer na
 sequência: Nadal(11), Gabriel(01), Federer(10), Nadal(00) 
 e novamente Nadal(11) para verificar se a rede aprendeu.
 """
+import math
 import numpy as np
 from random import uniform
 
 class Perceptron:
-	def __init__(self, threshold):
-		self.input = None
-		self.output = None
-		self.weights = None
-		self.threshold = threshold
+    def __init__(self):
+        self.weights = None
 
-	def activation_function(self):
-		result = (self.input * self.weights).sum()
-		print(self.input )
+    def active_function(self, inputs, threshold):
+    	result = (inputs * self.weights).sum()
+    	print("\n------------------------------------")
+    	print("Activation Function(input * weights) = ")
+    	print("input: ", inputs)
+    	print("weights: ", self.weights)
+    	print("threshold: ", threshold)
+    	print("input * weights: ", inputs * self.weights)
+    	print("result: ", result)
+    	print("---------------------------------------\n")
 
-
-		if result >= self.threshold:
-			self.output = -1
-		else: 
-			self.output =  1
-		print("\n------------------------------------")
-		print("Activation Function(input * weights) = ",self.output)
-		print("input: ", self.input)
-		print("weights: ", self.weights)
-		print("threshold: ", self.threshold)
-		print("input * weights: ", self.input * self.weights)
-		print("result: ", result)
-		print("---------------------------------------\n")
-
-		return self.output
-
-	def set_input(self, input):
-		self.input = np.array(input)
-
-	def set_weights(self, weights):
-		self.weights = np.array(weights)
-
-	def set_threshold(self, threshold):
-		self.threshold = threshold
-
-	def update_weights(self, learning_ratio, error):
-		print("\n------------------------------------")
-		print("UPDATING WEIGHTS")
-		print("Error: ", error)
-		print("Before weights: ", self.weights)
-		self.weights = self.weights + (learning_ratio * error * self.input)
-		print("After weigths: ", self.weights)
-		print("---------------------------------------\n")
-
+    	return int(math.tanh(result))
 
 class NeuralNetwork:
-	def __init__(self, size, threshold):
-		self.inputs = None
-		self.outputs = None
-		self.weights = None#[uniform(-1,1) for i in range(0, len(self.inputs[0])]
-		self.learning_ratio = None
-		self.perceptron = Perceptron(threshold)
+	def __init__(self, size):
+		self.perceptrons = [Perceptron() for i in range(0, size)]
 
-	def training(self, inputs,  outputs, learning_ratio, weights=None):
-		self.inputs = np.array(inputs)
-		self.outputs = outputs
-		self.learning_ratio = learning_ratio
-		self.weights = [uniform(-0.5,0.5) for i in range(0, len(inputs[0]))]
-		self.perceptron.set_weights(self.weights)
-		self.epocas = 0
-
+	def training(self, inputs, outputs, learning_ratio, threshold):
+		inputs = np.array(inputs)
+		outputs = np.array(outputs)
+		weights = None
+		for perceptron in self.perceptrons:
+			weights = np.array([uniform(-1, 1) for i in range(0, inputs.shape[1])])
+			perceptron.weights = weights
+		epoca = 0
 		print("===============TRAINING==================")
-		print("Input: ", self.inputs)
-		print("Desire Output: ", self.outputs)
-		print("Learning Ratio: ", self.learning_ratio)
-		print("Weights: ", self.weights)
+		print("Input: ", inputs)
+		print("Desire Output: ", outputs)
+		print("Learning Ratio: ", learning_ratio)
+		print("Weights: ", weights)
 		print("=========================================")
 
 		while True:
-			self.errors = np.array([False] * self.inputs.shape[0])
-			for index, i in enumerate(self.inputs):
-				self.perceptron.set_input(i)
-				if not self.perceptron.activation_function() == self.outputs[index]:
-					error = self.outputs[index] - self.perceptron.output
-					print("Ai")
-					print(error)
-					self.perceptron.update_weights(learning_ratio, error)
-					self.errors[index] = False
-				else:
-					self.errors[index] = True
-			self.epocas += 1
+			#errors = np.array([False] * inputs.shape[0])
+			errors = True
+			for index, i in enumerate(inputs):
+				for perceptron  in self.perceptrons:
+					output = perceptron.active_function(i, threshold)
+					if not output == outputs[index]:
+						error = outputs[index] - output
+						print("Ai")
+						print(error)
+						self.update_weigths(perceptron, inputs,learning_ratio, error)
+						#errors[index] = False
+						errors = errors and False
+					else:
+						errors = errors and True
+			epoca += 1
 			print("\n\nErrors: ")
-			print(self.errors)
+			print(errors)
 			print("\n\n")
-			input()
-			if np.all(self.errors):
+
+			if errors:
+				print("Ending training - 100 %")
+				break
+			elif epoca == 1000:
+				print("Atingiu o limite de épocas!")
 				break
 
-	def classify(self, input_t):
-		self.perceptron.set_input(input_t)
-		if self.perceptron.activation_function() == 1:
+	def update_weigths(self, perceptron, inputs, learning_ratio, error):
+		print("\n------------------------------------")
+		print("UPDATING WEIGHTS")
+		print("Error: ", error)
+		print("Before weights: ", perceptron.weights)
+		perceptron.weights = perceptron.weights + (learning_ratio * error * inputs)
+		print("After weigths: ", perceptron.weights)
+		print("---------------------------------------\n")
+
+	def classify(self, input_t, threshold):
+		perceptron = self.perceptrons[0]
+		if perceptron.active_function(input_t, threshold) == 0:
 			print(input_t , " -> Futebol")
 		else:
 			print(input_t , " -> Tenis")
 
 
 if __name__ == "__main__":
-	neural = NeuralNetwork(1, 0.5)
+	neural = NeuralNetwork(1)
+	bias = 0
 	inputs = [[0,0], [0, 1], [1,0],[1,1]]
-	outputs =[1, 1, -1, -1]
+	outputs =[0, 0, 1, 1]
 	learning_ratio = 0.1
-	neural.training(inputs, outputs, learning_ratio)
+	threshold = 0
+	neural.training(inputs, outputs, learning_ratio, threshold)
 	test  = [[1,1], [0,1], [1, 0], [0,0], [1,1]]
 	for i in test:
-		neural.classify(i)
+		neural.classify(i,threshold)
+
 
 
 
